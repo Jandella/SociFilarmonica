21fb037ff3be99afdf7194c52f71afbcb3b4e9fd
Jandella <chiara.deacetis@gmail.com>
2020-02-23 22:59:22 +0100

    Inizio creazione form calcolo rimborso

---
+++ SociFilarmonicaApp/SociFilarmonicaApp/Pages/Soci/CalcoloRimborso/Calcola.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SociFilarmonicaApp.Data;
+using SociFilarmonicaApp.ViewModels;
+
+namespace SociFilarmonicaApp
+{
+    public class CalcolaModel : PageModel
+    {
+        private readonly FilarmonicaContext _context;
+
+        public CalcolaModel(FilarmonicaContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CalcoloRimborsoVM DatiCalcolo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var socio = await _context.Soci
+                .Include(x => x.DatiAuto)
+                .FirstOrDefaultAsync(x => x.ID == id);
+
+            if (socio == null || socio.DatiAutoID == null)
+            {
+                return NotFound();
+            }
+
+            DatiCalcolo = new CalcoloRimborsoVM()
+            {
+                SocioID = socio.ID,
+                Cognome = socio.Cognome,
+                Nome = socio.Nome,
+                Carburante = socio.DatiAuto.Carburante,
+                DescrizioneMacchina = socio.DescrizioneMacchina,
+                Distanza = 0,
+                InfoAutoID = socio.DatiAutoID.Value,
+                RimborsoKm = socio.DatiAuto.RimborsoKm,
+                TargaMacchina = socio.TargaMacchina,
+                TipoAuto = socio.DatiAuto.TipoAuto
+            };
+            return Page();
+        }
+    }
+}
---
+++ SociFilarmonicaApp/SociFilarmonicaApp/ViewModels/CalcoloRimborsoVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SociFilarmonicaApp.ViewModels
+{
+    public class CalcoloRimborsoVM
+    {
+        public CalcoloRimborsoVM()
+        {
+            ListaProve = new List<DateTime>();
+            AltriCosti = new List<CalcoloRimborsoAltriCostiVM>();
+        }
+        public int SocioID { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        [Required]
+        [Display(Name = "Prove a cui ha partecipato")]
+        public List<DateTime> ListaProve { get; set; }
+        [Display(Name = "Numero prove")]
+        public int NumeroProve => ListaProve?.Count ?? 0;
+        [Display(Name = "Distanza casa-sede")]
+        public decimal Distanza { get; set; }
+        [Display(Name = "Targa")]
+        public string TargaMacchina { get; set; }
+        [StringLength(100)]
+        [Display(Name = "Modello veicolo")]
+        public string DescrizioneMacchina { get; set; }
+        public int InfoAutoID { get; set; }
+        [Display(Name = "Fascia rimborso")]
+        public string TipoAuto { get; set; }
+        public string Carburante { get; set; }
+        [Display(Name = "Rimborso per Km")]
+        public decimal RimborsoKm { get; set; }
+        public List<CalcoloRimborsoAltriCostiVM> AltriCosti { get; set; }
+        [Display(Name = "Totale")]
+        public decimal TotaleReale { get; set; }
+        [Display(Name = "Totale dovuto")]
+        public decimal TotaleDovuto { get; set; }
+    }
+    public class CalcoloRimborsoAltriCostiVM
+    {
+        public string Descrizione { get; set; }
+        public decimal Costo { get; set; }
+    }
+}
